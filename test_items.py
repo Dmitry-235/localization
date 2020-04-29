2c72f58a79c208a22e236c32250cbd97743f397f
Dmitry-235 <58150029+Dmitry-235@users.noreply.github.com>
2020-04-29 03:27:36 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+class TestMain():
+    def test_case(self, browser):
+        browser.get(link)
+        time.sleep(30)
+        browser.implicitly_wait(6)
+        button = browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket')
+        assert button, "Button find"
