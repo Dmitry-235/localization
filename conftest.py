b2731a737bf9df5ba7faed2d9be399dfcc749c9b
Dmitry-235 <58150029+Dmitry-235@users.noreply.github.com>
2020-04-29 14:48:28 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -16,12 +16,12 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    if (browser_name == "chrome"):
+    if browser_name == "chrome":
         print("\nstart chrome browser fro test...")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
-    elif (browser_name == "firefox"):
+    elif browser_name == "firefox":
         print("\nstart firefox browser for test...")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
